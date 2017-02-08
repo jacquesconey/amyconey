116c2addfe4614a979c1e2a2d11f204d5da6fa47
Jacques Coney <jacques.coney1@gmail.com>
2017-02-08 15:29:39 +0000

    created base structure for theme

---
+++ wordpress/wp-content/themes/customtheme/footer.php
@@ -0,0 +1,11 @@
+
+		<footer class="blog-footer">
+		    <p class="copyright">All of the images on the website are subject to Copyright &copy; 2017 Amy Coney</p>
+        </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<?php wp_footer(); ?>
+    </div>
+  </body>
+</html>
