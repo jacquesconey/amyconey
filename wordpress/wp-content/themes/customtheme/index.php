db554afb581e12e9518090a0be5aa69ccec802f1
Jacques Coney <jacques.coney1@gmail.com>
2017-02-09 17:25:23 +0000

    commented out unecessary code

--- wordpress/wp-content/themes/customtheme/index.php
+++ wordpress/wp-content/themes/customtheme/index.php
@@ -1,14 +1,16 @@
+<?php /* Template Name: index */ ?>
+
 <?php get_header(); ?>
 
 	<div class="row">
 
 		<div class="col-sm-8 blog-main">
 
-			<?php get_template_part( 'content', get_post_format() ); ?>
+			<!-- <?php //get_template_part( 'content', get_post_format() ); ?> -->
 
 		</div> <!-- /.blog-main -->
 
-		<?php get_sidebar(); ?>
+		<!-- <?php //get_sidebar(); ?> -->
 
 	</div> <!-- /.row -->
 
