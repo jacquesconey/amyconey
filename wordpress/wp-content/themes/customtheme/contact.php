a6684496a020286141ae852d3140164997a569f0
Jacques Coney <jacques.coney1@gmail.com>
2017-02-09 17:26:19 +0000

    updated contact page
    this was done as reset.css was also added which meant some standardisation was needed

---
+++ wordpress/wp-content/themes/customtheme/contact.php
@@ -0,0 +1,22 @@
+<?php /* Template Name: contact */ ?>
+
+<?php get_header(); ?>
+
+    <div class="content">
+        <div class="blurb">
+            <div class="about">
+                <h1>Amy Coney is a multi-discipline visual artist based in Manchester</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam quis rhoncus turpis. Nullam blandit accumsan nibh, eu tempus nunc posuere nec.</p><p> Pellentesque velit arcu, varius vitae fermentum id, fermentum vel felis. Praesent bibendum eleifend quam, <p> non rhoncus nibh iaculis at. Mauris sit amet laoreet ex. Nullam enim nunc, sollicitudin semper nisi ac, tempor laoreet ipsum. Maecenas varius in lacus ut bibendum. </p><p> Nam sit amet faucibus lorem. Etiam tempor turpis nisi, quis sollicitudin tortor vehicula non. Mauris vel lacus lorem. Etiam non leo rhoncus, imperdiet massa feugiat, pellentesque urna. Suspendisse elit mi, pharetra quis vulputate sit amet, mattis ut mi. Nulla facilisi. Quisque iaculis auctor lacus. Curabitur eget nunc eu est consectetur vulputate non vitae lectus.
+</p>
+            </div><div class="contact">
+                <p><strong>get in touch</strong></p>
+                <span class="contact-details"><strong>call: </strong>07976896267</span>
+                <span class="contact-details"><strong>email: </strong>amydconey@gmail.com</span>
+            </div>
+        </div>
+
+        <?php echo do_shortcode('[wdi_feed id="1"]'); ?>
+    </div>
+
+
+<?php get_footer(); ?>
